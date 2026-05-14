11491b8fa550814b273cd1457b35c122f8381512
ValentinoSandovalP <u20211a962@upc.edu.pe>
2026-05-14 00:48:03 -0500

    feat(index): export page components for chat, consultations, and recommendations

---
+++ src/modules/communication/index.ts
@@ -0,0 +1,6 @@
+// Presentation
+export { ChatPage } from "./presentation/pages/ChatPage";
+export { ConsultationsPage } from "./presentation/pages/ConsultationsPage";
+export { RecommendationsPage } from "./presentation/pages/RecommendationsPage";
+
+
